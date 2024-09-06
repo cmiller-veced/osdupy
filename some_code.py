90ceb0df6d7f5db8cd1ec40f078011f49e7d3481
cmiller-veced <cary.miller66@gmail.com>
2024-09-06 14:06:21 -0600

    validate jsonschema with $refs

--- some_code.py
+++ some_code.py
@@ -1,6 +1,10 @@
 import json
 import os
 
+import jsonschema
+from jsonschema import validate
+import jsonref
+
 
 pet_swagger = 'https://petstore.swagger.io/v2/swagger.json'
 pet_swagger_local = '~/local/petstore/swagger.json'
@@ -11,11 +15,43 @@ def raw_swagger(at_path):
         return json.load(fh)
 
 
-def go():
+def validate_jsonschema_with_refs():
+  try:
+    good_ones = [
+        {"name": 'kittyX', 'photoUrls': []},
+        {"name": 'kittyX', 'photoUrls': [], 'category': {}},
+        {"name": 'kittyX', 'photoUrls': [], 'status': 'sold'},
+        {"name": 'kittyX', 'photoUrls': [], 'category': {}, 'status': 'sold'},
+    ]
+    bad_ones = [
+        {},
+        {"name": 'kittyX'},
+        {"name": 'kittyX', 'photoUrls': [], 'category': ''}, 
+        {"name": 'kittyX', 'photoUrls': [], 'status': ''},
+    ]
     rs = raw_swagger(pet_swagger_local)
-    print(len(str(rs)))
-    print(len(rs))
+    with_refs = jsonref.loads(json.dumps(rs))
+    good_schema = with_refs['definitions']['Pet']  # The behavior we want
+    bad_schemas = [{}, dict(foo=2)]   # jsonschema allows any dict to be a schema.
+
+    for ob in good_ones:
+        validate(instance=ob, schema=good_schema)
+        print('ok good', ob)
+
+    for ob in bad_ones:
+        try:
+            validate(instance=ob, schema=good_schema)
+        except jsonschema.exceptions.ValidationError:
+            print('ok bad', ob)
+
+    for schema in bad_schemas:
+        validate(instance={}, schema=schema)
+        print('crap!')
+
+  finally:
+    globals().update(locals())
 
 
-go()
+validate_jsonschema_with_refs()
+
 
