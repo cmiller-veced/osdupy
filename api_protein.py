dd05b5631ddde48686a50cf80d23f9d9392e2169
cmiller-veced <cary.miller66@gmail.com>
2024-10-02 16:59:54 -0600

    tidy up

--- api_protein.py
+++ api_protein.py
@@ -1,46 +1,34 @@
-from protein_test_data import test_parameters
+from test_data_protein import test_parameters
 import json
 
-import httpx      # similar to R data frames
-import pandas      # similar to R data frames
-import jsonref     # cross platform
-import jsonschema     # cross platform
-from jsonschema import validate     # cross platform
-from jsonschema import (     # cross platform
-    Draft7Validator,
-    FormatChecker,
-)
+import httpx
+import jsonref
+from jsonschema import ( Draft7Validator, FormatChecker,)
 
 from tools import (
     raw_swagger, 
     local,        # not a tool.  It is data.
-    namespacify,
     endpoint_names,
     insert_endpoint_params,
 )
 from some_code import schema_trans
-
-from pprint import pprint
 
 #https://www.ncbi.nlm.nih.gov/genbank/samplerecord/
 #                    Examples: A2BC19, P12345, A0A023GPI8
 sample_query_params = {
     'accession': test_parameters['/proteins']['good'][0]['accession'],
 }
-
+head = {'accept': 'application/json'}  # some endpoints default to XML.
 
 # https://www.ebi.ac.uk/proteins/api/doc/#!/taxonomy/getTaxonomyLineageById
 # shows /taxonomy endpoints but these are not present in the OpenAPI file.
 
 
 def get_component_schemas_protein():    # EBI
-  try:
     rs = raw_swagger(local.swagger.protein)
     with_refs = jsonref.loads(json.dumps(rs))
     components = with_refs['components']
     return components['schemas']
-  finally:
-    globals().update(locals())
 
 
 def protein_validator(endpoint):
@@ -70,9 +58,6 @@ def protein_validator(endpoint):
     is_valid.endpoint = endpoint
     is_valid.schema = schema
     globals().update(locals())
-
-
-head = {'accept': 'application/json'}  # some endpoints default to XML.
 
 
 def protein_validate_and_call():
@@ -120,66 +105,8 @@ def protein_validate_and_call():
     globals().update(locals())
 
 
-# NOT specific to NWS except for base_url
-def nws_call(endpoint, params=None):
-    with httpx.Client(base_url=local.api_base.nws) as client:
-        r = client.get(endpoint, params=params)
-        assert r.status_code == 200
-    return r.json()
-
-
-# xxx data ##################################################################
-
-
-def nws_series():
-  try:
-    """ Get a series of observations suitable for putting in a pandas DF,
-    and then a jupyter notebook.
-    """
-    # Data
-    ep1 = '/stations/{stationId}/observations'
-    # TODO: validate stationId ??????
-#'089SE', '0900W'
-    stationId = 'KRCM'   # OK
-    stationId = 'CO100'   # OK
-    # NOTE  stationId cannot go in params.
-    params = {                                # OK
-        'start': '2024-09-22T23:59:59+00:00', 
-        'end':   '2024-09-23T23:59:59+00:00', 
-        'limit':   50,
-    }
-
-    # Validate params
-    assert nws_validator(ep1)(params)
-
-    # Insert stationId into endpoint path
-    ep = insert_endpoint_params(ep1, locals())
-
-    # Call the endpoint and verify status_code.
-    with httpx.Client(base_url=local.api_base.nws) as client:
-        r = client.get(ep, params=params)
-        assert r.status_code == 200
-
-    # Extract desired data from response.
-    final = []
-    feats = r.json()['features']
-    for ft in feats: 
-        pt = ft['properties']
-        for key in [ '@id', '@type', 'elevation', 'station', 'rawMessage', 'icon', 'presentWeather', 'cloudLayers', 'textDescription', ]:
-            pt.pop(key)
-        for key in pt:
-            if type(pt[key]) is dict:
-                pt[key] = pt[key]['value']
-        final.append(pt)
-
-    # Convert to dataframe.
-    df = pandas.DataFrame(final)
-#    assert df.shape == (50, 15)
-    assert df.shape[1] == 15
-    return df
-  finally:
-    globals().update(locals())
-
+# data
+# ############################################################################
 
 
 ps = get_component_schemas_protein()
@@ -188,6 +115,17 @@ pt = ps['ProteinType']
 st = ps['SequenceType']
 s = ps['Sequence']
 # Nice schemas.  Each entry in every schema has 'xml': {'attribute': True},
-# which code be eliminated w/o effect.
+# which could be eliminated w/o effect.
+
+
+# aside
+# ############################################################################
+
+# NOT specific to NWS except for base_url
+def nws_call(endpoint, params=None):
+    with httpx.Client(base_url=local.api_base.nws) as client:
+        r = client.get(endpoint, params=params)
+        assert r.status_code == 200
+    return r.json()
 
 
